88c5b623baf179b709f50db03948da4708453af9
Aman Rastogi <rastogiaman1998@gmail.com>
2020-06-02 22:49:21 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,57 @@
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Contact</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+<div class="modal-body">
+
+  <section class="text-gray-700 body-font relative">
+  <div class="absolute inset-0 bg-gray-300">
+    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1234.0496445530734!2d77.26294620784077!3d28.682667607535492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1591098964428!5m2!1sen!2sin"></iframe>
+  </div>
+  <div class="container px-5 py-24 mx-auto flex">
+    <div class=" bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact</h2>
+      <br>
+     
+      
+
+      <table>
+                            <td>Address:</td>
+                           <td>Delhi, Brahmpuri</td></tr>
+ 						   <tr>
+                        
+      
+      
+      
+                            <td>contact no:</td>
+                           <td>9811554026</td></tr>
+ 						   <tr>
+                        
+						   <td>Email:</td>
+						   <td>rastogiaman1998@gmail.com</td>
+                           </tr>
+    </table>
+
+
+     
+      
+    </div>
+  </div>
+</section>
+
+
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
---
+++ section/section1.php
@@ -0,0 +1,107 @@
+
+  <section class="text-gray-700 body-font">
+  <div class="container px-5 py-24 mx-auto flex flex-col">
+    
+     
+      <div class="flex flex-col sm:flex-row mt-10">
+        <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+          <div class=" rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+           
+              
+              <img class="avatar"src="image/profile.jpg">
+            
+          </div>
+          <div class="flex flex-col items-center text-center justify-center">
+            <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">Aman Rastogi</h2>
+            <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+            <p class="text-base text-gray-600">I am Web Desginer and Web Developer. I have experience in web development and customization.</p>
+          </div>
+          <br>
+          <div class="icon-box">
+          <a target="_black" href="https://www.facebook.com/Evil.9811/"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+          <a target="_black" href="#"><i class="fab fa-github" aria-hidden="true"></i></a>
+          <a target="_black" href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+          <a target="_black" href="https://www.instagram.com/aman_rastogi__/?hl=en"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+        </div>
+        </div>
+        <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-300 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+        
+
+ <ul class="nav nav-tabs" id="myTab" role="tablist" >
+                        <li class="nav-item">
+                           <a class="nav-link active" id="skills-tab" data-toggle="tab" href="#skills" role="tab" aria-controls="skills"
+                              aria-selected="true">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" id="work-tab" data-toggle="tab" href="#work" role="tab" aria-controls="work"
+                              aria-selected="false">Work Experience</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" id="knowledge-tab" data-toggle="tab" href="#knowledge" role="tab" aria-controls="knowledge"
+                              aria-selected="false">knowledge</a>
+                        </li>
+                        
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="skills" role="tabpanel" aria-labelledby="skills-tab">
+                                    <div class='tabs'style='margin-top:20px'> 
+                                    <div class="meter">
+                                            <label> HTML</label>
+                                          </div>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 95%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                          </div>
+                                          <div class="meter">
+                                            <label>CSS</label>
+                                          </div>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 95%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                          </div>
+                                          <div class="meter">
+                                            <label>PHP</label>
+                                          </div>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                          </div>
+                                          <div class="meter">
+                                            <label>JAVASCRIPT</label>
+                                          </div>
+                                          <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 69%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                          </div>
+                                    </div>
+                        </div>
+                        <div class="tab-pane fade" id="work" role="tabpanel" aria-labelledby="work-tab">
+                                    <div class='tabs'style='text-align:left; margin-top:20px'> 
+                                          <li>Work as a Intern at Vryx FutureTech</li>
+                                          <span class="mt-1 text-gray-500 text-sm">-as a Developer</span>
+                                          
+                                    </div>
+                        </div>
+                        <div class="tab-pane fade" id="knowledge" role="tabpanel" aria-labelledby="knowledge-tab">
+                                    <div class='tabs'style='text-align:left; margin-top:20px'> 
+                                          <li>JAVA</li>
+                                          <li>C LANGUAGE</li>
+                                          <li>PYTHON INTERMEDIATE</li>
+                                          <li>HTML CSS</li>
+                                          
+                                          
+                                                                                    
+                                    </div>
+                        </div>
+                        </div>
+
+        
+
+
+
+
+
+
+
+
+       </div>
+      </div>
+    </div>
+  
+</section>
